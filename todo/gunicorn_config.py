5775dbf966209619adc4e443d3e5e01996eed5b9
VincentJia <v39215068@gmail.com>
2024-03-31 17:46:34 +0800

    add gunicorn_confog best practice for prod

--- todo/gunicorn_config.py
+++ todo/gunicorn_config.py
@@ -1,5 +1,12 @@
+import multiprocessing
+
 bind = "0.0.0.0:8000"
-workers = 4
-max_requests = 1000
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"
 timeout = 30
-preload_app = True
+max_requests = 1000
+backlog = 2048
+debug = False
+logLevel = "debug"
+accesslog = "./logs/gunicorn_access.log"
+errorlog = "./logs/gunicorn_error.log"
