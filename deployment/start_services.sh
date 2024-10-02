6ab84d26e89cabf6f0b538dc1336a1bb65f45efc
VincentJia <v39215068@gmail.com>
2024-10-03 01:12:26 +0800

    feat: add start_services.sh script to start all containers and execute jobs automatically. This will replace docker-compose since using shell script can have more control and flexible.

---
+++ deployment/start_services.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Function to stop and remove a container if it exists
+function clean_container() {
+    if [ "$(docker ps -aq -f name=$1)" ]; then
+        echo "Stopping and removing existing container: $1"
+        docker stop $1
+        docker rm $1
+    fi
+}
+# Clean up existing containers
+clean_container todo-nginx
+clean_container todo-app
+clean_container todo-db
+
+# Create bridge network
+docker network create todo
+
+# Start PostgreSQL database
+echo "Starting PostgreSQL database..."
+docker run --name todo-db --network todo -p 5432:5432 -v todo-db-volume:/var/lib/postgresql/data -e POSTGRES_PASSWORD=123456 -d postgres:16
+# # Wait for PostgreSQL to become available
+# echo "Waiting for PostgreSQL to initialize..."
+# until docker exec todo-db pg_isready -U postgres; do
+#     sleep 1
+# done
+# Create the schema "todo"
+echo "Creating schema 'todo'..."
+docker exec todo-db psql -U postgres -c 'CREATE SCHEMA IF NOT EXISTS "todo";'
+
+# Build and start the Django app service
+echo "Building and starting Django app service..."
+docker build -t todo-app ./todo/.
+docker run --name todo-app --network todo -p 8000:8000 -d todo-app
+docker exec todo-app python manage.py migrate
+
+# Build and start Nginx
+echo "Building and starting Nginx..."
+docker build -t todo-nginx ./deployment/nginx/.
+docker run --name todo-nginx --network todo -p 80:80 -d todo-nginx
+
+echo "All services started successfully!"
