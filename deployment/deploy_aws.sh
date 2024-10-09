98e7552140716ffb705502511786305fa5c72bb6
VincentJia <v39215068@gmail.com>
2024-10-10 00:02:55 +0800

    add echo msg

--- deployment/deploy_aws.sh
+++ deployment/deploy_aws.sh
@@ -7,7 +7,10 @@ sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
+echo "Install Docker complete"
 newgrp docker
 
 # Start services. e.g. db, app, nginx
+echo "start service"
 sudo bash ./deployment/start_services.sh
+echo "start service complete"
